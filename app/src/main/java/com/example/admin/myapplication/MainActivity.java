83b5c0a5d13fb3b67314f4499a599e47fc04d362
SaMi <sami@gmail.com>
2018-11-25 00:24:05 +0100

    fuck you

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //chbik matosketch ie khraa
+        //chbik matosketch
         //test du git nnn
     }
 }
